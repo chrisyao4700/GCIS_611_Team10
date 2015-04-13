f503183845d2387befde34be0fa758fadaa8b7c0
dinesh-dev <sathasiv001@knights.gannon.edu>
2015-04-13 14:27:06 -0400

    from_junit
    Test files from junit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,28 @@
-
-public class HelloWorld {
+public class HelloWorld 
+{
 	
-	public static void main(String []args){
-		System.out.println("Hellow World!!!!");
-	}
+   private String name = "";
 
+   public String getName() 
+   {
+      return name;
+   }
+
+   public String getMessage() 
+   {
+      if (name == "") 
+      {
+         return "Hello!";
+      }
+      else 
+      {
+         return "Hello " + name + "!";
+      }
+   }
+
+   public void setName(String name) 
+   {
+      this.name = name;
+   }
+		
 }
